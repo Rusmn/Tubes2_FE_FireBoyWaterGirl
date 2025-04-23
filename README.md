472cb4675e4640fab7767a8380e85368b2947b6b
Rusmin <rusmn17@gmail.com>
2025-04-23 11:17:33 +0700

    setup FE

---
+++ README.md
@@ -0,0 +1,21 @@
+# TUBES2_FIREBOYWATERGIRL
+
+## Menjalankan
+
+1. Clone repo ini:
+   ```bash
+   git clone <URL-REPO-ANDA>
+   cd frontend
+   ```
+
+2. Install semua dependency:
+   ```bash
+   npm install
+   ```
+
+3. Jalankan server:
+   ```bash
+   npm run dev
+   ```
+
+Buka di browser: [http://localhost:5173](http://localhost:5173)
---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-300 flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-black mb-4">
+          TUBES 2 STIMA - TESTING TAILWIND + REACT + VITE
+        </h1>
+        <h2 className="text-5xl font-extrabold">
+          <span className="text-red-600">FIRE</span>
+          <span className="text-white">BOY</span>
+          <span className="text-blue-600">WATER</span>
+          <span className="text-pink-500">GIRL</span>
+        </h2>
+      </div>
+    </div>
+  )
+}
+
+export default App
